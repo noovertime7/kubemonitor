de61b80203c13dcc0ec9442fff9bd72b957172ab
chenteng <cc17854330572@163.com>
2023-10-12 10:38:24 +0800

    test: test

--- cmd/main.go
+++ cmd/main.go
@@ -28,8 +28,6 @@ import (
 	nativeZap "go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
